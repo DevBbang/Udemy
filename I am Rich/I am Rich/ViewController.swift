2b25cea8a706f83d8da40381336812211da0527a
DevBbang <ios_developer.th@icloud.com>
2022-02-08 22:18:03 +0900

    add first project

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by TAEJANIM on 2022/02/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
